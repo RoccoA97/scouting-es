d729c65bd5af4fef680d66a8a7324d3002eadb1a
Emilio <Emilio.Meschi@cern.ch>
2018-11-20 14:29:26 +0100

    modified formats and processor for new bx word structure

--- src/format.h
+++ src/format.h
@@ -13,73 +13,91 @@ struct block1{
   uint32_t mu2s[8];
 };
 
-// struct out_block1{
-//   uint32_t header;
-//   uint32_t bx;
-//   uint32_t orbit;
+struct muon{
+  uint32_t f;
+  uint32_t s;
+};
 
-//   // In the header is information of how many of muons are there
-//   uint32_t mu_w1;
-//   uint32_t mu_w2;
-// };
+struct block{
+  uint32_t bx;
+  uint32_t orbit;
+  muon mu[16];
+};
 
 struct masks{
-  static constexpr  uint32_t phiext = 0x1ff;
-  static constexpr  uint32_t pt = 0x1ff;
-  static constexpr  uint32_t qual = 0xf;
-  static constexpr  uint32_t etaext = 0x1ff;
-  static constexpr  uint32_t etaextv = 0xff;
-  static constexpr  uint32_t etaexts = 0x100;
-  static constexpr  uint32_t   iso = 0x3;
-  static constexpr  uint32_t   chrg = 0x1;
-  static constexpr  uint32_t   chrgv = 0x1;
-  static constexpr  uint32_t   index = 0x7f;
-  static constexpr  uint32_t   phi = 0x3ff;
-  static constexpr  uint32_t   eta = 0x1ff;
-  static constexpr  uint32_t   etav = 0xff;
-  static constexpr  uint32_t   etas = 0x100;
-  static constexpr  uint32_t  rsv = 0x3;
+  //bx word: 16 bits used for actual bx, MS 4 bits are muon type 
+  //(0xf intermediate, 0x0 final, following 4 bits for link id)
+  static constexpr  uint32_t bx      = 0xffff;
+  static constexpr  uint32_t interm  = 0x000f;
+  static constexpr  uint32_t linkid  = 0x000f;
+  //masks for muon 64 bits
+  static constexpr  uint32_t phiext  = 0x01ff;
+  static constexpr  uint32_t pt      = 0x01ff;
+  static constexpr  uint32_t qual    = 0x000f;
+  static constexpr  uint32_t etaext  = 0x01ff;
+  static constexpr  uint32_t etaextv = 0x00ff;
+  static constexpr  uint32_t etaexts = 0x0100;
+  static constexpr  uint32_t iso     = 0x0003;
+  static constexpr  uint32_t chrg    = 0x0001;
+  static constexpr  uint32_t chrgv   = 0x0001;
+  static constexpr  uint32_t index   = 0x007f;
+  static constexpr  uint32_t phi     = 0x03ff;
+  static constexpr  uint32_t eta     = 0x01ff;
+  static constexpr  uint32_t etav    = 0x00ff;
+  static constexpr  uint32_t etas    = 0x0100;
+  //NOTA BENE: reserved two bits are used for muon id
+  //0x1==intermediate, 0x2==final
+  static constexpr  uint32_t rsv     = 0x0003;
 };
 
 struct shifts{
-  static constexpr  uint32_t  phiext = 0;
-  static constexpr  uint32_t  pt = 10;
-  static constexpr  uint32_t  qual = 19;
+  //bx word: 16 bits used for actual bx, MS 4 bits are muon type 
+  //(0xf intermediate, 0x0 final, following 4 bits for link id)
+  static constexpr  uint32_t bx      = 0;
+  static constexpr  uint32_t interm  = 27;
+  static constexpr  uint32_t linkid  = 23;
+  //shifts for muon 64 bits
+  static constexpr  uint32_t  phiext =  0;
+  static constexpr  uint32_t  pt     = 10;
+  static constexpr  uint32_t  qual   = 19;
   static constexpr  uint32_t  etaext = 23;
-  static constexpr  uint32_t  iso =	0;
-  static constexpr  uint32_t  chrg = 2;
-  static constexpr  uint32_t  chrgv	= 3;
-  static constexpr  uint32_t  index	= 4;
-  static constexpr  uint32_t  phi = 11;
-  static constexpr  uint32_t  eta =	21;
-  static constexpr  uint32_t  rsv =	30;
+  static constexpr  uint32_t  iso    =  0;
+  static constexpr  uint32_t  chrg   =  2;
+  static constexpr  uint32_t  chrgv  =  3;
+  static constexpr  uint32_t  index  =  4;
+  static constexpr  uint32_t  phi    = 11;
+  static constexpr  uint32_t  eta    = 21;
+  static constexpr  uint32_t  rsv    = 30;
 };
 
 struct header_shifts{
-  static constexpr uint32_t bxmatch=24;
-  static constexpr uint32_t mAcount=16;
-  static constexpr uint32_t orbitmatch=8;
-  static constexpr uint32_t mBcount=0;
+  static constexpr uint32_t bxmatch    = 24;
+  static constexpr uint32_t mAcount    = 16;
+  static constexpr uint32_t orbitmatch =  8;
+  static constexpr uint32_t mBcount    =  0;
 };
 
 
 struct header_masks{
-  static constexpr uint32_t bxmatch = 0xff<<header_shifts::bxmatch;
-  static constexpr uint32_t mAcount = 0xf<<header_shifts::mAcount;
-  static constexpr uint32_t orbitmatch = 0xff<<header_shifts::orbitmatch;
-  static constexpr uint32_t mBcount = 0xf;
+  static constexpr uint32_t bxmatch    = 0xff << header_shifts::bxmatch;
+  static constexpr uint32_t mAcount    = 0x0f << header_shifts::mAcount;
+  static constexpr uint32_t orbitmatch = 0xff << header_shifts::orbitmatch;
+  static constexpr uint32_t mBcount    = 0x0f << header_shifts::mBcount;
 };
 
 struct gmt_scales{
-  static constexpr float pt_scale = 0.5;
+  static constexpr float pt_scale  = 0.5;
   static constexpr float phi_scale = 2.*M_PI/576.;
   static constexpr float eta_scale = 0.0870/8; //9th MS bit is sign
   static constexpr float phi_range = M_PI;
 };
 
 struct constants{
-  static constexpr uint32_t deadbeef = 0xdeadbeef;
+  static constexpr uint32_t deadbeef           = 0xdeadbeef;
+  static constexpr uint32_t intermediate_marker= 0x0000000f;
   static constexpr uint32_t orbit_trailer_size = 32;
+  static constexpr uint32_t intermediate       = 0x00000001;
+  static constexpr uint32_t final              = 0x00000001;
 };
 
 
