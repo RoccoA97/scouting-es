c07c622f0389de0a023f29a3a5bfee7426f5da9f
Petr Zejdl <petr.zejdl@cern.ch>
2018-12-03 17:27:45 +0100

    Allow to take data in run degraded mode

--- scripts/runControl.py
+++ scripts/runControl.py
@@ -5,8 +5,9 @@
 # Delay between two consecutive run control polls
 pollingDelay = 10
 
-# Acquire data in the following beam modes:
+# Acquire data in the following beam modes and DAQ states:
 allowedBeamModes = ['STABLE BEAMS', 'ADJUST']
+allowedDaqStates = ['RUNNING', 'RUNNINGDEGRADED']
 
 ## -----------------------
 
@@ -25,13 +26,13 @@ if __name__ == "__main__":
     while True:
         data=json.loads(urllib2.urlopen("http://daq-expert.cms:8081/DAQSnapshotService/getsnapshot?setup=cdaq").read())
         runNumber = data['runNumber']
-        beamMode = data['lhcBeamMode']
-        daqState = data['daqState']
+        beamMode = data['lhcBeamMode'].upper()
+        daqState = data['daqState'].upper()
         print "daq state ",daqState," beamMode ",beamMode," runNumber ",runNumber
 
         if True:
             # Check if LHC is running
-            if (beamMode in allowedBeamModes) and daqState=='Running':
+            if (beamMode in allowedBeamModes) and (daqState in allowedDaqStates):
                 print "going to start scouting for run ",runNumber
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_address = ('localhost', 8000)
@@ -53,7 +54,7 @@ if __name__ == "__main__":
 
         if True:
             # Check if LHC is not running
-            if (beamMode not in allowedBeamModes) or daqState != 'Running':
+            if (beamMode not in allowedBeamModes) or (daqState not in allowedDaqStates):
                 print "going to stop scouting for run ", current_run
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_address = ('localhost', 8000)
